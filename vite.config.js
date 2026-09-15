c2d9b70abb730653f7c86147e0f798b185c436f8
AsthaSingh-19 <asthasingh9c@gmail.com>
2026-09-16 00:56:02 +0530

    Fix Vercel blank screen

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/skin-care-app/', // for GitHub Pages: https://<username>.github.io/skin-care-app/
 })
